dba4c7b1ece5add72f6d7d9a037abb531ddb6c7e
Tonchavin Alexandr <bloodwayn@gmail.com>
2022-12-14 16:15:06 +1000

    add Examples005_If_Else

---
+++ Examples005_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Yup! This is Masha");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
